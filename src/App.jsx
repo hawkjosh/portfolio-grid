15ec8d157532747e487aedd2868717d0df998252
hawkjosh <hawkjosh@icloud.com>
2023-01-11 16:38:50 -0500

    content update push

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,170 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+import './assets/styles/App.css'
 
+import dumpsterFire from './assets/images/dumpster-fire.svg'
+import twitter from './assets/images/twitter.svg'
+import youtube from './assets/images/youtube.svg'
+import codepen from './assets/images/codepen.svg'
+import rightArrow from './assets/images/right-arrow.svg'
+import leftArrow from './assets/images/left-arrow.svg'
+
+export default function App() {
+
+  let activeIndex = 0
+
+  const slides = document.getElementsByTagName("article")
+  
+  const handleLeftClick = () => {
+    const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : slides.length - 1
+    
+    const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`)
+
+    const nextSlide = document.querySelector(`[data-index="${nextIndex}"]`)
+          
+    currentSlide.dataset.status = "after"
+    
+    nextSlide.dataset.status = "becoming-active-from-before"
+    
+    setTimeout(() => {
+      nextSlide.dataset.status = "active"
+      activeIndex = nextIndex
+    })
+  }
+
+  const handleRightClick = () => {
+    const nextIndex = activeIndex + 1 <= slides.length - 1 ? activeIndex + 1 : 0
+    
+    const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`)
+
+    const nextSlide = document.querySelector(`[data-index="${nextIndex}"]`)
+    
+    currentSlide.dataset.status = "before"
+    
+    nextSlide.dataset.status = "becoming-active-from-after"
+    
+    setTimeout(() => {
+      nextSlide.dataset.status = "active"
+      activeIndex = nextIndex
+    })
+  }
+  
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <nav data-toggled='false' data-transitionable='false'>
+
+        <div id='nav-logo-section' className='nav-section'>
+          <a href='#'>
+            <img src={dumpsterFire} alt='Dumpster Fire Icon' />
+          </a>
+        </div>
+
+        <div id='nav-link-section' className='nav-section'>
+          <a href='#'>ABOUT</a>
+          <a href='#'>WORK</a>
+        </div>
+
+        <div id='nav-social-section' className='nav-section'>
+          <a href='#'>
+            <img src={twitter} alt='Twitter Icon' />
+          </a>
+          <a href='#'>
+            <img src={youtube} alt='YouTube Icon' />
+          </a>
+          <a href='#'>
+            <img src={codepen} alt='Codepen Icon' />
+          </a>
+        </div>
+
+        <div id='nav-contact-section' className='nav-section'>
+          <a href='#'>GET IN TOUCH</a>
+        </div>
+
+      </nav>
+  
+      <main>
+
+        <article data-index='0' data-status='active'>
+          <div className='article-image-section article-section' />
+          <div className='article-info-section'>
+            <div className='article-title-section article-section'>
+              <h2>Slide 1</h2>
+            </div>
+            <div className='article-description-section article-section'>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit, omnis alias, exercitationem modi ipsum dolorem minus ratione sequi autem odit animi, labore laboriosam consequatur facilis delectus molestias.</p>
+            </div>
+            <div className='article-nav-section article-section'>
+              <button className='article-nav-button' type='button' onClick={handleLeftClick}>
+                <img src={leftArrow} alt='Left Arrow Icon' />
+              </button>
+              <button className='article-nav-button' type='button' onClick={handleRightClick}>
+                <img src={rightArrow} alt='Right Arrow Icon' />
+              </button>
+            </div>
+          </div>
+        </article>
+
+        <article data-index='1' data-status='inactive'>
+          <div className='article-image-section article-section' />
+          <div className='article-info-section'>
+            <div className='article-title-section article-section'>
+              <h2>Slide 2</h2>
+            </div>
+            <div className='article-description-section article-section'>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit, omnis alias, exercitationem modi ipsum dolorem minus ratione sequi autem odit animi, labore laboriosam consequatur facilis delectus molestias.</p>
+            </div>
+            <div className='article-nav-section article-section'>
+              <button className='article-nav-button' type='button' onClick={handleLeftClick}>
+                <img src={leftArrow} alt='Left Arrow Icon' />
+              </button>
+              <button className='article-nav-button' type='button' onClick={handleRightClick}>
+                <img src={rightArrow} alt='Right Arrow Icon' />
+              </button>
+            </div>
+          </div>
+        </article>
+
+        <article data-index='2' data-status='inactive'>    
+          <div className='article-image-section article-section' />
+          <div className='article-info-section'>
+            <div className='article-title-section article-section'>
+              <h2>Slide 3</h2>
+            </div>
+            <div className='article-description-section article-section'>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit, omnis alias, exercitationem modi ipsum dolorem minus ratione sequi autem odit animi, labore laboriosam consequatur facilis delectus molestias.</p>
+            </div>
+            <div className='article-nav-section article-section'>
+              <button className='article-nav-button' type='button' onClick={handleLeftClick}>
+                <img src={leftArrow} alt='Left Arrow Icon' />
+              </button>
+              <button className='article-nav-button' type='button' onClick={handleRightClick}>
+                <img src={rightArrow} alt='Right Arrow Icon' />
+              </button>
+            </div>
+          </div>
+        </article>
+
+        <article data-index='3' data-status='inactive'>    
+          <div className='article-image-section article-section' />
+          <div className='article-info-section'>
+            <div className='article-title-section article-section'>
+              <h2>Slide 4</h2>
+            </div>
+            <div className='article-description-section article-section'>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit, omnis alias, exercitationem modi ipsum dolorem minus ratione sequi autem odit animi, labore laboriosam consequatur facilis delectus molestias.</p>
+            </div>
+            <div className='article-nav-section article-section'>
+              <button className='article-nav-button' type='button' onClick={handleLeftClick}>
+                <img src={leftArrow} alt='Left Arrow Icon' />
+              </button>
+              <button className='article-nav-button' type='button' onClick={handleRightClick}>
+                <img src={rightArrow} alt='Right Arrow Icon' />
+              </button>
+            </div>
+          </div>
+        </article>
+
+      </main>
+    </>
   )
 }
-
-export default App
